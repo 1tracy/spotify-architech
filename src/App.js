5e36a2ea082c3cf4d55e47160f6e47db87fff641
tracy! <55264469+1tracy@users.noreply.github.com>
2021-10-09 00:45:46 -0400

    added charts.js and svg img

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React, { useState, useEffect, useRef } from 'react';
+import LineChart from './charts/chart';
+import { ReactComponent as LineGraph } from './vectors/landingpage.svg';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -11,11 +13,14 @@ function App() {
           <h1>My Spotify Architect</h1>
           <h2>Visualize your data</h2>
           <button onClick={() => setLoggedIn(true)}>Spotify Log In</button>
+          <LineGraph/>
         </div>
       )}
       {loggedIn && (
         <div className="App">
-          <h1>User logged in</h1>
+          <h1>My Data</h1>
+          <h2 className="top10tracks">My Top 10 Tracks </h2>
+          <LineChart />
           <button onClick={() => setLoggedIn(false)}>Log Out</button>
         </div>
       )}
