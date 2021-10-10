cce82ca4f784b25835a750c5e2caeeb8b0da3262
Helen Xia <helenyy.xia@gmail.com>
2021-10-09 20:22:33 -0400

    landing page styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState, useEffect, useRef } from 'react';
 import LineChart from './charts/chart';
-import { ReactComponent as LineGraph } from './vectors/landingpage.svg';
+import { ReactComponent as LineGraph } from './vectors/Background.svg';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -9,25 +9,27 @@ function App() {
   return (
     <>
       {!loggedIn &&  (
-        <div className="App">
-          <h1>My Spotify Architect</h1>
-          <h2>Visualize your data</h2>
-          <button onClick={() => setLoggedIn(true)}>Spotify Log In</button>
-          <div className="lineGraph">
-            <LineGraph/>
+        <body className = "landing">
+          <div className="App" >
+          <h1 className= "App-title" >My Spotify Architect</h1>
+          <h2 className= "App-description">Visualize your data</h2>
+          <button type = "button" class="signIn" onClick={() => setLoggedIn(true)}>Spotify Log In</button>
           </div>
-        </div>
+        </body>
+        
       )}
       {loggedIn && (
         <div className="App">
           <h1>My Data</h1>
           <div className="data">
             <h2 className="top10tracks">My Top 10 Tracks </h2>
+
             <div className="chart">
               <LineChart />
             </div>
+
           </div>
-          <button onClick={() => setLoggedIn(false)}>Log Out</button>
+          <button type = "button" class="signIn" onClick={() => setLoggedIn(false)}>Log Out</button>
         </div>
       )}
     </>
